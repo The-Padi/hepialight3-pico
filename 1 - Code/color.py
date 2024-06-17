60eb340fdb289ad34dcba2bcc1a50991374ad74b
michael.divia <michael.divia@etu.hesge.ch>
2024-06-17 17:08:08 +0200

    First try at Matrix function

---
+++ 1 - Code/color.py
@@ -0,0 +1,7 @@
+class Color:
+    BLACK = (0, 0, 0)
+    BLUE = (0, 0, 255)
+    CYAN = (0, 255, 255)
+    GREEN = (0, 255, 0)
+    MAGENTA = (255, 0, 255)
+    RED = (255, 0, 0)
--- 1 - Code/matrix.py
+++ 1 - Code/matrix.py
@@ -1,10 +1,11 @@
 from machine import Pin
 from neopixel import NeoPixel
-from matrix import matrix
 from color import color
 
 pin = Pin(0, Pin.OUT)   # set GPIO0 to output to drive NeoPixels
 np = NeoPixel(pin, 64)   # create NeoPixel driver on GPIO0 for 64 pixels
 
-while True:
-    matrix.clear(color.RED)
+class matrix:
+    def clear(self, color):
+        for i in range(0,64):
+            np[i] = color
