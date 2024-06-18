55cfca6710070ac1630c715e6a941e6217778a9a
michael.divia <michael.divia@etu.hesge.ch>
2024-06-18 11:24:34 +0200

    afficher_text function

--- 1 - Code/main.py
+++ 1 - Code/main.py
@@ -1,23 +1,4 @@
 from hl3 import *
 import time
 
-matrix.clear(0)
-
-matrix.set_led(5, 5, color.GREEN)
-print(matrix.get_led(5,5))
-
-for i in range(8):
-    matrix.set_line(i, color.RED)
-    time.sleep(0.1)
-    matrix.set_line(i, 0)
-
-for i in range(8):
-    matrix.set_column(i, color.RED)
-    time.sleep(0.1)
-    matrix.set_column(i, 0)
-    
-for i in range(8):
-    for j in range(8):
-        matrix.set_led(j, i, color.RED)
-        time.sleep(0.1)
-        matrix.set_led(j, i, 0)
+afficher_texte("Hello world!", color.ORANGE, 0.1)
